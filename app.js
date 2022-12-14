5ac6d70e7e8c2458bec3ac7a0353dd493cefe756
ByunJungMin <cvy5375@naver.com>
2022-12-14 09:53:07 +0900

    .

--- app.js
+++ app.js
@@ -3,9 +3,40 @@ const app = express();
 const port = 5000;
 const goodsRouter = require('./routes/goods.js')
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+app.use(express.json())
+
+app.post('/', (req, res) => {
+     console.log(req.body)
+
+     res.send("기본 URI에 POST 메소드가 정상적으로 실행 되었습니다.")
+})
+
+// app.get("/", (req,res) => {
+//      console.log(req.query);
+
+//      res.send('정상적으로 반환되었습니다.');
+// })
+
+app.get("/", (req,res) => {
+     console.log(req.query);
+
+     const obj = {
+          "KeyKey": "value 입니다.",
+          "이름입니다": "이름일까요?"
+     }
+
+     res.json(obj)
+})
+
+app.get("/:id", (req,res) => {
+     console.log(req.params)
+
+     res.send(':id URI에 정상적으로 반환되었습니다.')
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.use('/api', goodsRouter)
 
